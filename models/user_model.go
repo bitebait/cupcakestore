ea19b5d53b42903da78504437cd888fbd771ca73
bitebait <william@schwaab.me>
2023-10-13 16:59:50 +0200

    add FirstLogin and LastLogin field

--- models/user_model.go
+++ models/user_model.go
@@ -1,17 +1,20 @@
 package models
 
 import (
+	"time"
+
 	"github.com/go-playground/validator/v10"
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"type:varchar(100);" validate:"required"`
-	Email    string `gorm:"type:varchar(100);unique_index" validate:"required,email"`
-	Password string `gorm:"type:varchar(100);" validate:"required,min=8"`
-	IsActive bool   `gorm:"default:true"`
-	IsStaff  bool   `gorm:"default:false"`
+	Username  string    `gorm:"type:varchar(100);" validate:"required"`
+	Email     string    `gorm:"type:varchar(100);unique_index" validate:"required,email"`
+	Password  string    `gorm:"type:varchar(100);" validate:"required,min=8"`
+	IsActive  bool      `gorm:"default:true"`
+	IsStaff   bool      `gorm:"default:false"`
+	LastLogin time.Time `gorm:"type:timestamp"`
 }
 
 func (u *User) Validate() error {
