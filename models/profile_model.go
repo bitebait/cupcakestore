baaa7d347dfc064122c3e3876033de7fe4f90b5a
bitebait <william@schwaab.me>
2023-10-27 19:27:12 -0400

    add profile phone number

--- models/profile_model.go
+++ models/profile_model.go
@@ -7,13 +7,14 @@ import (
 
 type Profile struct {
 	gorm.Model
-	FirstName  string `gorm:"type:varchar(100)"`
-	LastName   string `gorm:"type:varchar(100)"`
-	Address    string `gorm:"type:varchar(200)"`
-	City       string `gorm:"type:varchar(100)"`
-	State      string `gorm:"type:varchar(100)"`
-	PostalCode string `gorm:"type:varchar(20)"`
-	UserID     uint
+	FirstName   string `gorm:"type:varchar(100)"`
+	LastName    string `gorm:"type:varchar(100)"`
+	Address     string `gorm:"type:varchar(200)"`
+	City        string `gorm:"type:varchar(100)"`
+	State       string `gorm:"type:varchar(100)"`
+	PostalCode  string `gorm:"type:varchar(20)"`
+	PhoneNumber string `gorm:"type:varchar(20)"`
+	UserID      uint
 }
 
 func (p *Profile) Validate() error {
