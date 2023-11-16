69dd789cd04fd9b09d150fb2c10ec4e9df112624
bitebait <william@schwaab.me>
2023-11-16 15:12:39 -0300

    remove profile validator

--- models/profile_model.go
+++ models/profile_model.go
@@ -1,14 +1,13 @@
 package models
 
 import (
-	"github.com/go-playground/validator/v10"
 	"gorm.io/gorm"
 )
 
 type Profile struct {
 	gorm.Model
-	FirstName   string `gorm:"type:varchar(100)" validate:"required"`
-	LastName    string `gorm:"type:varchar(100)" validate:"required"`
+	FirstName   string `gorm:"type:varchar(100)"`
+	LastName    string `gorm:"type:varchar(100)"`
 	Address     string `gorm:"type:varchar(200)"`
 	City        string `gorm:"type:varchar(100)"`
 	State       string `gorm:"type:varchar(100)"`
@@ -20,17 +19,4 @@ type Profile struct {
 
 func (p *Profile) FullName() string {
 	return p.FirstName + " " + p.LastName
-}
-
-func (p *Profile) BeforeSave(tx *gorm.DB) error {
-	return p.Validate()
-}
-
-func (p *Profile) BeforeCreate(tx *gorm.DB) error {
-	return p.Validate()
-}
-
-func (p *Profile) Validate() error {
-	v := validator.New()
-	return v.Struct(p)
 }
