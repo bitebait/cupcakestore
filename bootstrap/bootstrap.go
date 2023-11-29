11bd4f7d1ccaefd7740d12e771a1997443841578
bitebait <william@schwaab.me>
2023-11-29 20:40:47 -0300

    disable cache-control

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -33,7 +33,6 @@ func NewApplication() *fiber.App {
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
-	app.Use(middlewares.CacheControl())
 	app.Use(recover.New())
 	app.Use(logger.New())
 	app.Use(compress.New(compress.Config{
