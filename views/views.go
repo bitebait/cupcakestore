39dd3d22fe605f81bf2c07214278ea5c9e171c88
bitebait <william@schwaab.me>
2024-06-01 17:09:26 -0300

    feat(view): improve view renderer

--- views/views.go
+++ views/views.go
@@ -4,8 +4,21 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func Render(ctx *fiber.Ctx, templateName string, data interface{}, message string, baseLayout ...string) error {
+const (
+	BaseLayout  = "layouts/base"
+	StoreLayout = "layouts/store"
+)
+
+func render(ctx *fiber.Ctx, templateName string, data interface{}, message string, baseLayout ...string) error {
 	ctx.Locals("Error", message != "")
 	ctx.Locals("Message", message)
 	return ctx.Render(templateName, fiber.Map{"Object": data}, baseLayout...)
+}
+
+func Render(ctx *fiber.Ctx, template string, data interface{}, baseLayout ...string) error {
+	return render(ctx, template, data, "", baseLayout...)
+}
+
+func RenderError(ctx *fiber.Ctx, template string, data interface{}, message string, baseLayout ...string) error {
+	return render(ctx, template, data, message, baseLayout...)
 }
