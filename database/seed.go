f6dccc9a4ad40fc5cee8ae81201b73fcafd7e510
bitebait <william@schwaab.me>
2024-06-01 23:21:48 -0300

    feat(database): update seed script to use GORM

--- database/seed.go
+++ database/seed.go
@@ -1,101 +1,54 @@
 package database
 
 import (
-	"fmt"
-	"log"
-	"time"
-
 	"github.com/bitebait/cupcakestore/models"
 	"gorm.io/gorm"
+	"log"
 )
 
 type Seeder interface {
 	Seed(db *gorm.DB) error
 }
 
-type ProfileAdminSeeder struct{}
-
-func (s ProfileAdminSeeder) Seed(db *gorm.DB) error {
-	query := `
-        INSERT INTO profiles (id, created_at, updated_at, deleted_at, first_name, last_name, address, city, state, postal_code, phone_number, user_id)
-        SELECT ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
-        WHERE NOT EXISTS (
-            SELECT 1 FROM profiles WHERE id = ?
-        )
-    `
-
-	result := db.Exec(query,
-		2,
-		time.Now(),
-		time.Now(),
-		nil,
-		"Administrador",
-		"Demo",
-		"Rua M D 840",
-		"Foz Do Iguacu",
-		"Paraná",
-		"22222-222",
-		"11 1111 1111",
-		5,
-		2,
-	)
-
-	if result.Error != nil {
-		fmt.Println("Error executing query:", result.Error)
-		return result.Error
-	}
-	return nil
-}
-
 type UserAdminSeeder struct{}
 
 func (s UserAdminSeeder) Seed(db *gorm.DB) error {
-	query := `
-     INSERT INTO users (id, created_at, updated_at, deleted_at, email, password, is_active, is_staff, first_login, last_login)
-        SELECT ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
-        WHERE NOT EXISTS (
-            SELECT 1 FROM users WHERE id = ?
-        )
-    `
-	result := db.Exec(query,
-		5,
-		time.Now(),
-		time.Now(),
-		nil,
-		"admin@admin.com",
-		"$2a$10$DcchYSRWj4ikydnA5XNTv.5o4jmmM.pltIlSI8foKiL321w5t66Wi",
-		1,
-		1,
-		time.Time{},
-		time.Time{},
-		5,
-	)
-
-	if result.Error != nil {
-		fmt.Println("Error executing query:", result.Error)
-		return result.Error
+	admin := &models.User{
+		Email:    "admin@admin.com",
+		Password: "admin@admin.com",
+		IsActive: true,
+		IsStaff:  true,
 	}
+
+	if err := db.FirstOrCreate(&admin, "email = ?", admin.Email).Error; err != nil {
+		log.Fatalf("Failed to create AdminUser: %v", err)
+		return err
+	}
+
 	return nil
 }
 
 type StoreConfigSeeder struct{}
 
 func (s StoreConfigSeeder) Seed(db *gorm.DB) error {
-	var count int64
-
-	if err := db.Model(&models.StoreConfig{}).Count(&count).Error; err != nil {
-		log.Fatalf("Erro ao contar registros de StoreConfig: %v", err)
+	storeConfig := &models.StoreConfig{
+		DeliveryPrice:            10,
+		DeliveryIsActive:         true,
+		PhysicalStoreEmail:       "foo@bar.com",
+		PhysicalStoreAddress:     "Foo Bar",
+		PhysicalStoreCity:        "Foo Bar",
+		PhysicalStoreState:       "Foo Bar",
+		PhysicalStorePostalCode:  "00000-000",
+		PhysicalStorePhoneNumber: "(00)00000-0000",
+		PaymentCashIsActive:      true,
+		PaymentPixIsActive:       true,
+		PixKey:                   "000.000.000-00",
+		PixKeyType:               models.PixTypeCPF,
 	}
 
-	if count == 0 {
-		storeConfig := models.StoreConfig{
-			PixKeyType: models.PixTypeCNPJ,
-		}
-
-		if err := db.Create(&storeConfig).Error; err != nil {
-			log.Fatalf("Falha ao criar StoreConfig: %v", err)
-			return err
-		}
+	if err := db.FirstOrCreate(&storeConfig, "physical_store_email = ?", storeConfig.PhysicalStoreEmail).Error; err != nil {
+		log.Fatalf("Failed to create StoreConfig: %v", err)
+		return err
 	}
 
 	return nil
@@ -103,9 +56,8 @@ func (s StoreConfigSeeder) Seed(db *gorm.DB) error {
 
 func SeedDatabase(db *gorm.DB) error {
 	seeders := []Seeder{
-		StoreConfigSeeder{},
 		UserAdminSeeder{},
-		ProfileAdminSeeder{},
+		StoreConfigSeeder{},
 	}
 
 	for _, seeder := range seeders {
