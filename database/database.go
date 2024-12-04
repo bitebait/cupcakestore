9c85d3df1ac91173d07b50cd30f42dfb69d2cf72
bitebait <william@schwaab.me>
2024-12-03 21:41:03 -0300

    fix: update PostgreSQL DSN environment config string

--- database/database.go
+++ database/database.go
@@ -2,15 +2,13 @@ package database
 
 import (
 	"fmt"
-	"log"
-	"os"
-
 	"github.com/bitebait/cupcakestore/config"
 	"github.com/bitebait/cupcakestore/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	"log"
 )
 
 var (
@@ -42,15 +40,10 @@ func setupSQLiteDatabase() {
 }
 
 func setupPostgresDatabase() {
+	cfg := config.Get()
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_SSLMODE"),
-		os.Getenv("DB_TIMEZONE"),
+		cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort, cfg.DBSSLMode,
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
